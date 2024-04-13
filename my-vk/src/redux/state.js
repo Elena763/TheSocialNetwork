acbe2a5c4ba258dd7b8541330ec7c8707c9341e3
Elena763 <elena.evg@bk.ru>
2024-04-13 19:40:08 +0400

    Added dispath

--- my-vk/src/redux/state.js
+++ my-vk/src/redux/state.js
@@ -31,29 +31,32 @@ let store = {
         },*/
 
     },
-    getState() {
-        return this._state;
-    },
     _callSubscriber() {
         console.log(this._state);
     },
-    addPost () {
-        let newPost = {
-            id: 5,
-            message: this._state.profilePage.newPostText,
-            likesCount: 0,
-        };
 
-        this._state.profilePage.posts.push(newPost);
-        this._state.profilePage.newPostText="";
-        this._callSubscriber(this._state);
-    },
-    updateNewPostText (newText) {
-        this._state.profilePage.newPostText = newText;
-        this._callSubscriber(this._state);
+    getState() {
+        return this._state;
     },
     subscribe (observer) {
         this._callSubscriber = observer; //observer - pattern
+    },
+
+    dispatch(action){
+        if (action.type === 'ADD-POST') {
+            let newPost = {
+                id: 5,
+                message: this._state.profilePage.newPostText,
+                likesCount: 0,
+            };
+
+            this._state.profilePage.posts.push(newPost);
+            this._state.profilePage.newPostText="";
+            this._callSubscriber(this._state);
+        } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+            this._state.profilePage.newPostText = action.newText;
+            this._callSubscriber(this._state);
+        }
     },
 }
 
