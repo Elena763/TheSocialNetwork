ccc7ab6ace2e532bdef101ef495393b25c4da22b
Elena763 <elena.evg@bk.ru>
2024-05-07 13:54:49 +0400

    Modified Users

--- my-vk/src/components/Users/Users.jsx
+++ my-vk/src/components/Users/Users.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import usersStyle from './Users.module.css';
+import axios from "axios";
+import userPic from "../../assets/images/ProfileImg.png";
 
 function Users(props) {
 
-    if (props.users.lenght === 0) {
-        props.setUsers( [
+    if (props.users.length === 0) {
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+          .then(response => {
+            props.setUsers(response.data.items);
+        });
+      }
+       /*
+        [
           {id: 1, userpic: 'https://mrandmrs50plus.com/wp-content/uploads/2018/02/smiling-woman-2.jpg',
             followed: false, fullname: 'Elena', status: "Hello world!", location: {country: 'Russia', city: 'Samara'}},
           {id: 2, userpic: 'https://gallamed.ru/upload/iblock/ee6/ee6969e643c6bb47242b383b5ab32380.png',
@@ -13,9 +22,9 @@ function Users(props) {
             followed: false, fullname: 'Renata', status: "world!", location: {country: 'Russia', city: 'Ufa'}},
           {id: 4, userpic: 'https://455811.selcdn.ru/lovemother/uploads/2018/01/215.jpg',
             followed: true, fullname: 'Stasy', status: "Horld!", location: {country: 'Vietnam', city: 'NhaTrang'}},
-      ]
-    )
-  }
+            'https://social-network.samuraijs.com/users'
+          ]*/
+
 
     return (
       <div className={usersStyle.s}>
@@ -23,7 +32,7 @@ function Users(props) {
           props.users.map (u => <div key={u.id}>
         <div className={usersStyle.s}>
             <div className={usersStyle.userpic}>
-              <img src={u.userpic} alt="pic" />
+              <img src={ u.photos.small != null ? u.photos.small : userPic } alt="pic" />
             </div>
             <div className={usersStyle.s}>
               { u.followed
@@ -33,12 +42,12 @@ function Users(props) {
         </div>
         <div className={usersStyle.s}>
           <div className={usersStyle.s}>
-            <div className={usersStyle.s}>{u.fullname}</div>
+            <div className={usersStyle.s}>{u.name}</div>
             <div className={usersStyle.s}>{u.status}</div>
           </div>
           <div className={usersStyle.s}>
-            <div className={usersStyle.s}>{u.location.country}</div>
-            <div className={usersStyle.s}>{u.location.city}</div>
+            <div className={usersStyle.s}>{'u.location.country'}</div>
+            <div className={usersStyle.s}>{'u.location.city'}</div>
           </div>
         </div>
         </div>)
