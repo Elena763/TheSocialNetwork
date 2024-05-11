1ccfed83aab923293e5a3f3f40e62b64bdc5053b
Elena763 <elena.evg@bk.ru>
2024-05-11 21:54:14 +0400

    Modified styles for Users

--- my-vk/src/components/Users/Users.jsx
+++ my-vk/src/components/Users/Users.jsx
@@ -28,41 +28,48 @@ class Users extends React.Component {
     for ( let i=1; i<=pagesCount; i++) {
       pages.push(i);
 
-    }
+    };
+    let curP = this.props.currentPage;
+    let curPF = ((curP - 5) < 0) ?  0  : curP - 5 ;
+    let curPL = curP + 5;
+    let slicedPages = pages.slice( curPF, curPL);
+
 
     return (
-      <div className={usersStyle.s}>
-        <div>
-          {pages.map( p => {
+      <div className={usersStyle.usersPage}>
+        <div className={usersStyle.pages}>
+          {slicedPages.map( p => {
             return <span className={this.props.currentPage === p && usersStyle.selectedPage}
             onClick={ (e) => {this.onPageChanged(p); }}>{p + " "}</span>
           })}
         </div>
+
         {
-          this.props.users.map (u => <div key={u.id}>
-        <div className={usersStyle.s}>
+          this.props.users.map (u => <div className={usersStyle.user_wrapper} key={u.id}>
+        <div className={usersStyle.user}>
             <div className={usersStyle.userpic}>
               <img src={ u.photos.small != null ? u.photos.small : userPic } alt="pic" />
             </div>
             <div className={usersStyle.s}>
               { u.followed
-              ? <button onClick={() => {this.props.unfollow(u.id)}} >Unfollow</button>
-              : <button onClick={() => {this.props.follow(u.id)}} >Follow</button>}
+              ? <button className={usersStyle.button_unfollow} onClick={() => {this.props.unfollow(u.id)}} >Unfollow</button>
+              : <button className={usersStyle.button} onClick={() => {this.props.follow(u.id)}} >Follow</button>}
             </div>
         </div>
-        <div className={usersStyle.s}>
+        <div className={usersStyle.userInfo}>
           <div className={usersStyle.s}>
-            <div className={usersStyle.s}>{u.name}</div>
-            <div className={usersStyle.s}>{u.status}</div>
+            <div className={usersStyle.name}>{u.name}</div>
+            <div className={usersStyle.status}>{u.status}</div>
           </div>
           <div className={usersStyle.s}>
-            <div className={usersStyle.s}>{'u.location.country'}</div>
-            <div className={usersStyle.s}>{'u.location.city'}</div>
+            <div className={usersStyle.country}>{'u.location.country'}</div>
+            <div className={usersStyle.city}>{'u.location.city'}</div>
           </div>
         </div>
         </div>)
         }
-      </div>
+        </div>
+
     );
   }
 }
