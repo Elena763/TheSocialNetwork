83c048af33cff6e7fda6b525fd728116296e174f
Elena763 <elena.evg@bk.ru>
2024-05-13 15:31:24 +0400

    Modified Users, IsersContainer. Improved Users - CleanComponent

--- my-vk/src/components/Users/Users.jsx
+++ my-vk/src/components/Users/Users.jsx
@@ -1,59 +1,40 @@
 import React from "react";
 import usersStyle from './Users.module.css';
-import axios from "axios";
 import userPic from "../../assets/images/ProfileImg.png";
 
-class Users extends React.Component {
+function Users(props)  {
 
-  componentDidMount() {
-      axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-        .then(response => {
-          this.props.setUsers(response.data.items);
-          this.props.setTotalUsersCount(response.data.totalCount);
-      });
-  }
-  onPageChanged = (pageNumber) => {
-    this.props.setCurrentPage(pageNumber);
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
-        .then(response => {
-          this.props.setUsers(response.data.items);
-      });
-  }
+  let pagesCount = Math.ceil (props.totalUsersCount / props.pageSize);
+  let pages = [];
+  for ( let i=1; i<=pagesCount; i++) {
+    pages.push(i);
+  };
+  let curP = props.currentPage;
+  let curPF = ((curP - 5) < 0) ?  0  : curP - 5 ;
+  let curPL = curP + 5;
+  let slicedPages = pages.slice( curPF, curPL);
 
-
-  render () {
-
-    let pagesCount = Math.ceil (this.props.totalUsersCount / this.props.pageSize);
-    let pages = [];
-    for ( let i=1; i<=pagesCount; i++) {
-      pages.push(i);
-
-    };
-    let curP = this.props.currentPage;
-    let curPF = ((curP - 5) < 0) ?  0  : curP - 5 ;
-    let curPL = curP + 5;
-    let slicedPages = pages.slice( curPF, curPL);
-
-
-    return (
+  return (
       <div className={usersStyle.usersPage}>
         <div className={usersStyle.pages}>
           {slicedPages.map( p => {
-            return <span className={this.props.currentPage === p && usersStyle.selectedPage}
-            onClick={ (e) => {this.onPageChanged(p); }}>{p + " "}</span>
+            return <span className={props.currentPage === p && usersStyle.selectedPage}
+                onClick={ (e) => {
+                  props.onPageChanged(p);
+                }}>{p + " "}</span>
           })}
         </div>
 
         {
-          this.props.users.map (u => <div className={usersStyle.user_wrapper} key={u.id}>
+          props.users.map (u => <div className={usersStyle.user_wrapper}  key={u.id}>
         <div className={usersStyle.user}>
             <div className={usersStyle.userpic}>
               <img src={ u.photos.small != null ? u.photos.small : userPic } alt="pic" />
             </div>
             <div className={usersStyle.s}>
               { u.followed
-              ? <button className={usersStyle.button_unfollow} onClick={() => {this.props.unfollow(u.id)}} >Unfollow</button>
-              : <button className={usersStyle.button} onClick={() => {this.props.follow(u.id)}} >Follow</button>}
+              ? <button className={usersStyle.button_unfollow} onClick={() => {props.unfollow(u.id)}} >Unfollow</button>
+              : <button className={usersStyle.button} onClick={() => {props.follow(u.id)}} >Follow</button>}
             </div>
         </div>
         <div className={usersStyle.userInfo}>
@@ -72,5 +53,5 @@ class Users extends React.Component {
 
     );
   }
-}
-  export default Users;
+
+export default Users;
