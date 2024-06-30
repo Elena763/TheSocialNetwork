87cfc13a802b8c4b634eee2f3fceab2d4472eadd
Elena763 <elena.evg@bk.ru>
2024-06-30 22:00:32 +0400

    fixed bug

--- my-vk/src/App.js
+++ my-vk/src/App.js
@@ -25,7 +25,7 @@ function App(props) {
               element={<DialogsContainer
                 store={props.store}
                 />} />
-            <Route path='/profile/*'
+            <Route path='/profile/:id'
               element={<ProfileContainer
                 store={props.store}
                 />} />
--- my-vk/src/components/Profile/ProfileContainer.jsx
+++ my-vk/src/components/Profile/ProfileContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import Profile from "./Profile.jsx";
 import { getUserProfile, getStatus, updateStatus } from "../../redux/profileReducer.js";
@@ -8,19 +8,20 @@ import { compose } from "@reduxjs/toolkit";
 
 function ProfileContainer(props) {
 
-    let [userID, setUserID] = useState();
+    let params = useParams();
+    let userID = params.id;
     if (!userID) {
       userID=2;
     };
-    alert(userID);
     useEffect(() => {
-      getUserProfile(userID);
-      getStatus(userID);
+      props.getUserProfile(userID);
+      props.getStatus(userID);
     },[userID]);
 
     if (!props.isAuth) {
       return <Navigate to={'/login'}/>;
     };
+
     return (
       <Profile {...props} profile={props.profile} status={props.status} updateStatus={props.updateStatus}/>
     );
